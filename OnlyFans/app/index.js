17373117092fff70bccc7173af7f2dbfca0e8eda
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-04-10 18:32:02 +0300

    started

--- OnlyFans/app/index.js
+++ OnlyFans/app/index.js
@@ -1,34 +1,12 @@
-import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
-import users from "../assets/data/users";
-
-const user = users[1];
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={{ uri: user.coverImage }}
-        style={styles.userCard}
-      >
-        <View style={styles.overlay} />
-
-        {/* image */}
-        <Image style={styles.userImage} src={user.avatar} />
-        {/* name & handle */}
-        <View>
-          <Text
-            style={{
-              fontWeight: "bold",
-              color: "white",
-              fontSize: 22,
-              marginBottom: 5,
-            }}
-          >
-            {user.name}
-          </Text>
-          <Text style={{ color: "white", fontSize: 16 }}>@{user.handle}</Text>
-        </View>
-      </ImageBackground>
+      <View style={styles.main}>
+        <Text style={styles.title}>Hello World</Text>
+        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+      </View>
     </View>
   );
 }
@@ -36,28 +14,22 @@ export default function Page() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
-    paddingTop: 75,
-    backgroundColor: "#00a4ef",
+    alignItems: "center",
+    padding: 24,
   },
-  overlay: {
-    backgroundColor: "rgba(0,0,0,0.2)",
-    ...StyleSheet.absoluteFillObject,
+  main: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
   },
-  userCard: {
-    flexDirection: "row",
-    padding: 10,
-    backgroundColor: "#ff8900",
-    alignItems: "flex-end",
-    borderRadius: 10,
-    overflow: "hidden",
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
   },
-  userImage: {
-    width: 100,
-    height: 100,
-    borderRadius: 100,
-    borderColor: "white",
-    borderWidth: 3,
-    marginRight: 12,
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
   },
 });
