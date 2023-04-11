d03f152f6f2c5c5e0a8257a3238e43c56f333016
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-04-11 21:38:22 +0300

    changes_data

--- OnlyFans/assets/data/users.js
+++ OnlyFans/assets/data/users.js
@@ -2,9 +2,9 @@ export default [
   {
     id: "1",
     coverImage:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+      "https://images.pexels.com/photos/935756/pexels-photo-935756.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     avatar:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
+      "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     name: "Alice Smith",
     handle: "alice_smith",
     bio: "Comedian and content creator, sharing my daily adventures and making you laugh along the way.",
@@ -13,9 +13,9 @@ export default [
   {
     id: "2",
     coverImage:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+      "https://images.pexels.com/photos/821742/pexels-photo-821742.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     avatar:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg",
+      "https://images.pexels.com/photos/1758845/pexels-photo-1758845.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     name: "Bob Johnson",
     handle: "bob_johnson",
     bio: "Professional prankster and master of laughter, join me for the best comedy content around.",
@@ -24,9 +24,9 @@ export default [
   {
     id: "3",
     coverImage:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/2.jpg",
+      "https://images.pexels.com/photos/207247/pexels-photo-207247.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     avatar:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/biahaze.jpg",
+      "https://images.pexels.com/photos/3808103/pexels-photo-3808103.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     name: "Cathy Brown",
     handle: "cathy_brown",
     bio: "Lifestyle and comedy vlogger, sharing the lighter side of life through fun videos and challenges.",
@@ -35,9 +35,9 @@ export default [
   {
     id: "4",
     coverImage:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/3.jpg",
+      "https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     avatar:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
+      "https://images.pexels.com/photos/1391498/pexels-photo-1391498.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     name: "David Wilson",
     handle: "david_wilson",
     bio: "Your go-to guy for the funniest reactions, commentary, and hilarious skits.",
@@ -46,9 +46,9 @@ export default [
   {
     id: "5",
     coverImage:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/4.jpg",
+      "https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     avatar:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/graham.jpg",
+      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     name: "Eva Green",
     handle: "eva_green",
     bio: "Stand-up comedian and humor enthusiast, creating content that will leave you in stitches.",
@@ -57,9 +57,9 @@ export default [
   {
     id: "6",
     coverImage:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/5.jpeg",
+      "https://images.pexels.com/photos/1030979/pexels-photo-1030979.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     avatar:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg",
+      "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     name: "Frank Taylor",
     handle: "frank_taylor",
     bio: "Funny videos, relatable content, and epic comedy challenges. Join the fun!",
@@ -68,9 +68,9 @@ export default [
   {
     id: "7",
     coverImage:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/6.jpg",
+      "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     avatar:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg",
+      "https://images.pexels.com/photos/3769713/pexels-photo-3769713.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     name: "Grace Lee",
     handle: "grace_lee",
     bio: "Laugh along with my daily antics, comedic sketches, and light-hearted take on life.",
@@ -79,9 +79,9 @@ export default [
   {
     id: "8",
     coverImage:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/7.jpeg",
+      "https://images.pexels.com/photos/1040626/pexels-photo-1040626.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     avatar:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG",
+      "https://images.pexels.com/photos/1445465/pexels-photo-1445465.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     name: "Harry Baker",
     handle: "harry_baker",
     bio: "Comedy sketches, parodies, and feel-good content to brighten your day.",
@@ -90,9 +90,9 @@ export default [
   {
     id: "9",
     coverImage:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/5.jpeg",
+      "https://images.pexels.com/photos/417362/pexels-photo-417362.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     avatar:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg",
+      "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     name: "Isla Adams",
     handle: "isla_adams",
     bio: "Join me for a daily dose of laughter, funny stories, and heartwarming moments.",
