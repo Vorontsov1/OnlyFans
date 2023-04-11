820aff69a5cb7e70636d6e5f5857267ef734acd6
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-04-11 12:08:13 +0300

    updae_data

--- OnlyFans/assets/data/users.js
+++ OnlyFans/assets/data/users.js
@@ -5,10 +5,10 @@ export default [
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
     avatar:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
-    name: "Elon",
-    handle: "elonmusk",
-    bio: "Only Funs is an entertainment platform dedicated to bringing joy and laughter to your life! We offer a wide variety of content, from hilarious sketches and memes to light-hearted challenges and games.",
-    subscriptionPrice: 4.99,
+    name: "Alice Smith",
+    handle: "alice_smith",
+    bio: "Comedian and content creator, sharing my daily adventures and making you laugh along the way.",
+    subscriptionPrice: 9.99,
   },
   {
     id: "2",
@@ -16,10 +16,10 @@ export default [
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
     avatar:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg",
-    name: "Jeff",
-    handle: "jeffbezos",
-    bio: "Only Funs is an entertainment platform dedicated to bringing joy and laughter to your life! We offer a wide variety of content, from hilarious sketches and memes to light-hearted challenges and games.",
-    subscriptionPrice: 4.99,
+    name: "Bob Johnson",
+    handle: "bob_johnson",
+    bio: "Professional prankster and master of laughter, join me for the best comedy content around.",
+    subscriptionPrice: 12.99,
   },
   {
     id: "3",
@@ -27,10 +27,10 @@ export default [
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/2.jpg",
     avatar:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/biahaze.jpg",
-    name: "Biahaze",
-    handle: "biahaze",
-    bio: "Only Funs is an entertainment platform dedicated to bringing joy and laughter to your life! We offer a wide variety of content, from hilarious sketches and memes to light-hearted challenges and games.",
-    subscriptionPrice: 2.99,
+    name: "Cathy Brown",
+    handle: "cathy_brown",
+    bio: "Lifestyle and comedy vlogger, sharing the lighter side of life through fun videos and challenges.",
+    subscriptionPrice: 7.99,
   },
   {
     id: "4",
@@ -38,10 +38,10 @@ export default [
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/3.jpg",
     avatar:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
-    name: "Elon",
-    handle: "elon2musk",
-    bio: "Only Funs is an entertainment platform dedicated to bringing joy and laughter to your life! We offer a wide variety of content, from hilarious sketches and memes to light-hearted challenges and games.",
-    subscriptionPrice: 1.99,
+    name: "David Wilson",
+    handle: "david_wilson",
+    bio: "Your go-to guy for the funniest reactions, commentary, and hilarious skits.",
+    subscriptionPrice: 10.99,
   },
   {
     id: "5",
@@ -49,9 +49,9 @@ export default [
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/4.jpg",
     avatar:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/graham.jpg",
-    name: "Graham",
-    handle: "grahamdave",
-    bio: "Only Funs is an entertainment platform dedicated to bringing joy and laughter to your life! We offer a wide variety of content, from hilarious sketches and memes to light-hearted challenges and games.",
+    name: "Eva Green",
+    handle: "eva_green",
+    bio: "Stand-up comedian and humor enthusiast, creating content that will leave you in stitches.",
     subscriptionPrice: 0,
   },
   {
@@ -60,10 +60,10 @@ export default [
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/5.jpeg",
     avatar:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg",
-    name: "Jeff",
-    handle: "jeff2bazos",
-    bio: "Only Funs is an entertainment platform dedicated to bringing joy and laughter to your life! We offer a wide variety of content, from hilarious sketches and memes to light-hearted challenges and games.",
-    subscriptionPrice: 3.99,
+    name: "Frank Taylor",
+    handle: "frank_taylor",
+    bio: "Funny videos, relatable content, and epic comedy challenges. Join the fun!",
+    subscriptionPrice: 11.99,
   },
   {
     id: "7",
@@ -71,10 +71,10 @@ export default [
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/6.jpg",
     avatar:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg",
-    name: "Vadim",
-    handle: "vadim",
-    bio: "Only Funs is an entertainment platform dedicated to bringing joy and laughter to your life! We offer a wide variety of content, from hilarious sketches and memes to light-hearted challenges and games.",
-    subscriptionPrice: 1.99,
+    name: "Grace Lee",
+    handle: "grace_lee",
+    bio: "Laugh along with my daily antics, comedic sketches, and light-hearted take on life.",
+    subscriptionPrice: 9.49,
   },
   {
     id: "8",
@@ -82,9 +82,20 @@ export default [
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/7.jpeg",
     avatar:
       "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG",
-    name: "Vadim",
-    handle: "vadim2savin",
-    bio: "Only Funs is an entertainment platform dedicated to bringing joy and laughter to your life! We offer a wide variety of content, from hilarious sketches and memes to light-hearted challenges and games.",
+    name: "Harry Baker",
+    handle: "harry_baker",
+    bio: "Comedy sketches, parodies, and feel-good content to brighten your day.",
     subscriptionPrice: 0,
+  },
+  {
+    id: "9",
+    coverImage:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/5.jpeg",
+    avatar:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg",
+    name: "Isla Adams",
+    handle: "isla_adams",
+    bio: "Join me for a daily dose of laughter, funny stories, and heartwarming moments.",
+    subscriptionPrice: 6,
   },
 ];
