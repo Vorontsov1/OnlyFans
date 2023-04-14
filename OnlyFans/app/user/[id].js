2fa6944bf68d7f5a39bea51242a6ad35865eafeb
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-04-14 12:07:15 +0300

    add_hide_subscribe

--- OnlyFans/app/user/[id].js
+++ OnlyFans/app/user/[id].js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import UserProfileHeader from '../../src/components/UserProfileHeader';
 import posts from '../../assets/data/posts';
 import Post from '../../src/components/Post';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const ProfilePage = () => {
 const [subScribed, setSubScribed] = useState(false); 
@@ -13,8 +14,43 @@ const [subScribed, setSubScribed] = useState(false);
 
   const user = users.find((u) => u.id === id);
   if (!user) {
-    return <Text>User not found</Text>
+    return <Text>User not found!</Text>
   };
+if (!subScribed) {
+  return (
+    <View>
+      <UserProfileHeader
+        user={user}
+        subScribed={subScribed}
+        setSubScribed={setSubScribed}
+      />
+      <View
+        style={{
+          backgroundColor: "gainsboro",
+          padding: 20,
+          alignItems: "center",
+        }}
+      >
+        <MaterialCommunityIcons name="lock-off" size={50} color="grey" />
+        <Text
+          style={{
+            fontWeight: "bold",
+            fontSize: 20,
+            backgroundColor: "#0496FF",
+            padding: 10,
+            color: "white",
+            borderRadius: 20,
+            overflow: "hidden",
+            fontSize: 20,
+            alignItems: "center",
+          }}
+        >
+          Subscribe to see user`s posts
+        </Text>
+      </View>
+    </View>
+  );
+}
 
   return (
       <FlatList
