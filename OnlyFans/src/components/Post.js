a30ee4222fd22d039015c342a44734e3351d2397
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-04-14 11:54:14 +0300

    fixed_post

--- OnlyFans/src/components/Post.js
+++ OnlyFans/src/components/Post.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
-import { Entypo } from "@expo/vector-icons";
+import { Entypo, AntDesign, Feather } from "@expo/vector-icons";
 
 const Post = ({ post }) => {
   return (
@@ -30,14 +30,56 @@ const Post = ({ post }) => {
             @{post.User.handle}
           </Text>
         </View>
-        <View style={{ marginLeft: "auto", flexDirection: "row", alignItems: "center" }}>
-          <Text style={{ color: "gray", fontSize: 16, marginRight: 5 }}>3 hours ago</Text>
-          <Entypo name="dots-three-horizontal" size={18 } color="grey" />
+        <View
+          style={{
+            marginLeft: "auto",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "gray", fontSize: 16, marginRight: 5 }}>
+            3 hours ago
+          </Text>
+          <Entypo name="dots-three-horizontal" size={18} color="grey" />
         </View>
       </View>
 
-          <Image src={post.image} style={{ width: "100%", aspectRatio: 1 }} />
-          <Text style={{ color: "gray", fontSize: 16, margin: 10, lineHeight: 18, letterSpacing: 0.8, }}>{post.text}</Text> 
+      <Text
+        style={{
+          color: "gray",
+          fontSize: 16,
+          margin: 7,
+          lineHeight: 18,
+          letterSpacing: 0.8,
+        }}
+      >
+        {post.text}
+      </Text>
+      <Image src={post.image} style={{ width: "100%", aspectRatio: 1 }} />
+      <View style={{ margin: 10, flexDirection: "row" }}>
+        <AntDesign
+          style={{ marginRight: 15 }}
+          name="like2"
+          size={20}
+          color="grey"
+        />
+        <Feather
+          name="dollar-sign"
+          size={20}
+          color="grey"
+          style={{ marginRight: 15 }}
+        />
+      </View>
+      <Text
+        style={{
+          color: "gray",
+          fontSize: 16,
+          fontWeight: "500",
+          marginHorizontal: 10,
+        }}
+      >
+        {post.likes} Likes
+      </Text>
     </View>
   );
 };
